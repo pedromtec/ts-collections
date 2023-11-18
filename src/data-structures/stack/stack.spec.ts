26a9ce0d88d3c1ad69683d53f139bb403210b60a
Pedro Soares <pedromtec@gmail.com>
2023-11-18 15:00:25 -0300

    feat: Add first tests

--- src/data-structures/stack/stack.spec.ts
+++ src/data-structures/stack/stack.spec.ts
@@ -1,3 +1,18 @@
+import { Stack } from '.'
+
 describe('Stack', () => {
-  it('should push an item on the top of the stack', () => {})
+  it('should push an item on the top of the stack', () => {
+    const stack = new Stack<number>()
+    stack.push(1)
+    stack.push(2)
+    expect(stack.peek()).toBe(2)
+  })
+
+  it('should remove the last item pushed', () => {
+    const stack = new Stack<string>()
+    stack.push('Apple')
+    stack.push('Food')
+
+    expect(stack.pop()).toBe('Food')
+  })
 })
