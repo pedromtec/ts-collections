109f57ce1921867739d8896552cf4a476c04a507
Pedro Soares <pedromtec@gmail.com>
2023-11-20 20:34:08 -0300

    feat: Add queue data structure

--- src/data-structures/queue/index.ts
+++ src/data-structures/queue/index.ts
@@ -1,1 +1,15 @@
-console.log('Queue')
+export class Queue<T> {
+  #items: T[] = []
+
+  add(item: T) {
+    this.#items.push(item)
+  }
+
+  remove() {
+    return this.#items.shift()
+  }
+
+  peek() {
+    return this.#items[0]
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './data-structures/stack'
+export * from './data-structures/queue'
