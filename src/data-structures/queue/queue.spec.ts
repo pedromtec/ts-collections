6543976895e67547ee5447a4897368f333f1bded
Pedro Soares <pedromtec@gmail.com>
2023-11-24 19:57:06 -0300

    feat: Add search method in the Queue

--- src/data-structures/queue/queue.spec.ts
+++ src/data-structures/queue/queue.spec.ts
@@ -14,4 +14,24 @@ describe('Queue', () => {
 
     expect(queue.remove()).toBe(3)
   })
+
+  it('should search for an item', () => {
+    const queue = new Queue<number>()
+
+    queue.add(1)
+    queue.add(2)
+    queue.add(3)
+
+    expect(queue.search(2)).toBe(1)
+  })
+
+  it('should return -1 if the item does not exists', () => {
+    const queue = new Queue<number>()
+
+    queue.add(1)
+    queue.add(2)
+    queue.add(3)
+
+    expect(queue.search(5)).toBe(-1)
+  })
 })
